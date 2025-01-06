b09750adc32080fcd81dda4ea7f13b1a0f2e5499
Devels. <109744785+kujs02029@users.noreply.github.com>
2025-01-06 10:25:04 -0600

    Initial Commit

--- WoundClassification.py
+++ WoundClassification.py
@@ -24,8 +24,8 @@ os.makedirs(test_folder, exist_ok=True)
 subfolders = []
 
 for f in os.scandir(main_folder):
-    if f.is_dir():
-        subfolders.append(f.path)
+  if f.is_dir():
+      subfolders.append(f.path)
 
 #split train / val / test
 train_perc = 80
@@ -33,39 +33,39 @@ val_perc = 10
 
 for subfolder in subfolders:
 
-    # extract folder name
-    subfolder_name = os.path.basename(subfolder)
+  # extract folder name
+  subfolder_name = os.path.basename(subfolder)
 
-    #create the train, validation, and test subfolder
-    train_subfolder = os.path.join(train_folder , subfolder_name)
-    val_subfolder = os.path.join(val_folder, subfolder_name)
-    test_subfolder = os.path.join(test_folder , subfolder_name)
+  #create the train, validation, and test subfolder
+  train_subfolder = os.path.join(train_folder , subfolder_name)
+  val_subfolder = os.path.join(val_folder, subfolder_name)
+  test_subfolder = os.path.join(test_folder , subfolder_name)
 
-    os.makedirs(train_subfolder, exist_ok=True)
-    os.makedirs(val_subfolder, exist_ok=True)
-    os.makedirs(test_subfolder, exist_ok=True)
+  os.makedirs(train_subfolder, exist_ok=True)
+  os.makedirs(val_subfolder, exist_ok=True)
+  os.makedirs(test_subfolder, exist_ok=True)
 
-    #list all files in the current subfolder
-    files = [f.path for f in os.scandir(subfolder) if f.is_file()]
+  #list all files in the current subfolder
+  files = [f.path for f in os.scandir(subfolder) if f.is_file()]
 
-    #shuffle the files
-    random.shuffle(files)
+  #shuffle the files
+  random.shuffle(files)
 
-    #calculate the number of train files
-    num_train_files = int(len(files) * (train_perc / 100))
-    num_val_files = int(len(files) * (val_perc / 100))
-    print(num_train_files)
+  #calculate the number of train files
+  num_train_files = int(len(files) * (train_perc / 100))
+  num_val_files = int(len(files) * (val_perc / 100))
+  print(num_train_files)
 
-    #copy the files to the train folder
-    for file in files[:num_train_files]:
-        shutil.copy(file, os.path.join(train_subfolder, os.path.basename(file)))
+  #copy the files to the train folder
+  for file in files[:num_train_files]:
+      shutil.copy(file, os.path.join(train_subfolder, os.path.basename(file)))
 
-    for file in files[num_train_files:num_train_files+num_val_files]:
-        shutil.copy(file, os.path.join(val_subfolder, os.path.basename(file)))
+  for file in files[num_train_files:num_train_files+num_val_files]:
+      shutil.copy(file, os.path.join(val_subfolder, os.path.basename(file)))
 
-    #copy the files to the test folder
-    for file in files[num_train_files + num_val_files:]:
-        shutil.copy(file, os.path.join(test_subfolder, os.path.basename(file)))
+  #copy the files to the test folder
+  for file in files[num_train_files + num_val_files:]:
+      shutil.copy(file, os.path.join(test_subfolder, os.path.basename(file)))
 
 print("Finished copying the files to Train, Validation, and Test subfolder")
 
@@ -85,12 +85,12 @@ import matplotlib.pyplot as plt
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        print("GPU memory growth enabled")
-    except RuntimeError as e:
-        print(e)
+  try:
+      for gpu in gpus:
+          tf.config.experimental.set_memory_growth(gpu, True)
+      print("GPU memory growth enabled")
+  except RuntimeError as e:
+      print(e)
 
 IMAGE_SIZE = 224 # all the images will be resized to this size
 
@@ -114,94 +114,105 @@ base_model.trainable = True
 #create a new model with more layers for our data
 
 model = keras.Sequential([
-    base_model,
-    #layers.Flatten(),
-    layers.GlobalAveragePooling2D(),
-    layers.Dense(1024, activation = 'relu', kernel_regularizer= keras.regularizers.l2(7e-3)),
-    layers.BatchNormalization(), #remove this if not successful
-    layers.Dropout(0.5),
-    layers.Dense(512, activation='relu', kernel_regularizer= keras.regularizers.l2(6e-3)),
-    layers.BatchNormalization(),
-    layers.Dropout(0.3),
-    layers.Dense(256, activation='relu', kernel_regularizer= keras.regularizers.l2(5e-3)),
-    layers.BatchNormalization(),
-    layers.Dropout(0.3),
-    layers.Dense(num_classes, activation='softmax')
+  base_model,
+  #layers.Flatten(),
+  layers.GlobalAveragePooling2D(),
+  layers.Dense(1024, activation = 'relu', kernel_regularizer= keras.regularizers.l2(5e-3)),
+  layers.BatchNormalization(), #remove this if not successful
+  layers.Dropout(0.5),
+  layers.Dense(512, activation='relu', kernel_regularizer= keras.regularizers.l2(4e-3)),
+  layers.BatchNormalization(),
+  layers.Dropout(0.3),
+  layers.Dense(256, activation='relu', kernel_regularizer= keras.regularizers.l2(3e-3)),
+  layers.BatchNormalization(),
+  layers.Dropout(0.3),
+  layers.Dense(num_classes, activation='softmax')
 ])
 
 from keras.optimizers import Adam
+
 adam_opt = Adam(learning_rate=1e-4) # low value for transfer learning
 
 model.compile(optimizer = adam_opt, loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 #load the data
 train_datagen = ImageDataGenerator(
-    preprocessing_function=keras.applications.efficientnet_v2.preprocess_input, #x
-    rescale=1./255,
-    shear_range= 0.3,
-    zoom_range=0.4,
-    horizontal_flip=True,
-    rotation_range=50,
-    width_shift_range=0.3,
-    height_shift_range=0.3,
-    brightness_range=(0.9,1.1), #x
-    channel_shift_range=0.2, #x
-    fill_mode = 'nearest'
+  preprocessing_function=keras.applications.efficientnet_v2.preprocess_input, #x
+  rescale=1./255,
+  shear_range= 0.25,
+  zoom_range=0.3,
+  horizontal_flip=True,
+  rotation_range=50,
+  width_shift_range=0.25,
+  height_shift_range=0.25,
+  brightness_range=(0.9,1.1), #x
+  channel_shift_range=0.15, #x
+  fill_mode = 'nearest'
 )
 
 test_datagen = ImageDataGenerator(rescale= 1./255)
 
 train_generator = train_datagen.flow_from_directory(
-    train_folder,
-    target_size=(IMAGE_SIZE,IMAGE_SIZE),
-    batch_size=8,
-    class_mode='categorical',
-    color_mode='rgb',
-    shuffle=True
+  train_folder,
+  target_size=(IMAGE_SIZE,IMAGE_SIZE),
+  batch_size=8,
+  class_mode='categorical',
+  color_mode='rgb',
+  shuffle=True
 )
 
 val_generator = test_datagen.flow_from_directory(
-    val_folder,
-    target_size=(IMAGE_SIZE,IMAGE_SIZE),
-    batch_size=8,
-    class_mode='categorical',
-    color_mode='rgb',
-    shuffle=False
+  val_folder,
+  target_size=(IMAGE_SIZE,IMAGE_SIZE),
+  batch_size=8,
+  class_mode='categorical',
+  color_mode='rgb',
+  shuffle=False
 )
 
 test_generator = test_datagen.flow_from_directory(
-    test_folder,
-    target_size=(IMAGE_SIZE,IMAGE_SIZE),
-    batch_size=8,
-    class_mode='categorical',
-    color_mode='rgb',
-    shuffle=False
+  test_folder,
+  target_size=(IMAGE_SIZE,IMAGE_SIZE),
+  batch_size=8,
+  class_mode='categorical',
+  color_mode='rgb',
+  shuffle=False
 )
 
 EPOCHS = 300
 best_model_file = 'E:\WoundProject\data_v2-EfficientNetV2.keras'
 
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, LearningRateScheduler
-
-def lr_schedule(epoch, lr): #x
-    if epoch < 10:
-        return lr
-    elif epoch < 70:
-        return lr * 0.25
-    else:
-        return lr * 0.1
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 callbacks = [
-    ModelCheckpoint(best_model_file, verbose=1, save_best_only=True, monitor="val_accuracy"),
-    ReduceLROnPlateau(monitor="val_accuracy", patience=8,factor=0.4,verbose=1, min_lr=1e-8),
-    #original: ReduceLROnPlateau(monitor="val_accuracy", patience=10,factor=0.1,verbose=1, min_lr=1e-6),
-    EarlyStopping(monitor="val_accuracy", patience=38,verbose=1, restore_best_weights=True),
-    #original: EarlyStopping(monitor="val_accuracy", patience=25,verbose=1)
-    #LearningRateScheduler(lr_schedule) #x
+  ModelCheckpoint(best_model_file, verbose=1, save_best_only=True, monitor="val_accuracy"),
+  ReduceLROnPlateau(monitor="val_accuracy", patience=8,factor=0.4,verbose=1, min_lr=1e-8),
+  #original: ReduceLROnPlateau(monitor="val_accuracy", patience=10,factor=0.1,verbose=1, min_lr=1e-6),
+  EarlyStopping(monitor="val_accuracy", patience=30,verbose=1, restore_best_weights=True),
+  #original: EarlyStopping(monitor="val_accuracy", patience=25,verbose=1)
 ]
 
+from sklearn.utils.class_weight import compute_class_weight
+
+# Get class indices from the train generator
+class_indices = train_generator.class_indices
+
+# Convert class indices to a sorted list of class labels
+class_labels = list(class_indices.keys())
+
+# Get class counts from the generator
+class_counts = train_generator.classes
+
+# Compute class weights
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(class_counts),y=class_counts)
+
+# Convert to dictionary format
+class_weights_dict = {i: weight for i, weight in enumerate(class_weights)}
+
+print("Class Weights:", class_weights_dict)
+
 result = model.fit(
-    train_generator, epochs = EPOCHS, validation_data= val_generator, callbacks = callbacks
+  train_generator, epochs = EPOCHS, validation_data= val_generator, callbacks = callbacks, class_weight= class_weights_dict
 )
 
 # get the index of the epoch with the highest validation accuracy
@@ -254,8 +265,8 @@ predictions = model.predict(test_generator)
 confusion = confusion_matrix(true_labels, np.argmax(predictions, axis = -1))
 
 # Plot the confusion matrix
-plt.figure(figsize=(20, 12))
-sns.heatmap(confusion, annot=True, fmt="d", cmap="Blues", cbar=False, xticklabels=class_names, yticklabels=class_names)
+plt.figure(figsize=(8, 6))
+sns.heatmap(confusion / np.sum(confusion, axis=1)[:, np.newaxis], annot=True, fmt=".2%", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
 plt.xlabel("Predicted Labels")
 plt.ylabel("True Labels")
 plt.title("Confusion Matrix")
